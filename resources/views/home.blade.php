814dd74631ee6f78c8956b881cfa67521030449d
Fabuzac <fabuzac.novenaire@hotmail.com>
2022-02-24 15:15:10 +0100

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -962,7 +962,7 @@
 					<div class="ctg-right">
 						<a href="#" target="_blank">
 							<img class="img-fluid d-block mx-auto" src="img/category/c5.jpg" alt="">
-						</a>
+						</a> 
 					</div>
 				</div>
 			</div>
