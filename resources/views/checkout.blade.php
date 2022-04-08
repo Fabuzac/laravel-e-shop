873f932defe055cd5a69b095351abc95fc9ff3ae
Fabuzac <fabuzac.novenaire@hotmail.com>
2022-04-08 18:03:53 +0200

    paypal integration...

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -265,50 +265,34 @@
                     <script>
                         
                         paypal.Buttons({
-                        // Sets up the transaction when a payment button is clicked
-                            
-                        createOrder: (data, actions) => {
+                        // Sets up the transaction when a payment button is clicked                            
+                            createOrder: (data, actions) => {
 
-                        return actions.order.create({
+                            return actions.order.create({
 
-                            purchase_units: [{
+                                purchase_units: [{
 
-                            amount: {
+                                amount: {
+                                    value: '77.44' // Can also reference a variable or function
+                                }
+                                }]
+                            });
+                            },
 
-                                value: '77.44' // Can also reference a variable or function
+                            // Finalize the transaction after payer approval
+                            onApprove: (data, actions) => {
 
-                            }
+                            return actions.order.capture().then(function(orderData) {
 
-                            }]
+                                // Successful capture! For dev/demo purposes:
 
-                        });
+                                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
 
-                        },
+                                const transaction = orderData.purchase_units[0].payments.captures[0];
 
-                        // Finalize the transaction after payer approval
+                                alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
 
-                        onApprove: (data, actions) => {
-
-                        return actions.order.capture().then(function(orderData) {
-
-                            // Successful capture! For dev/demo purposes:
-
-                            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-
-                            const transaction = orderData.purchase_units[0].payments.captures[0];
-
-                            alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
-
-                            // When ready to go live, remove the alert and show a success message within this page. For example:
-
-                            // const element = document.getElementById('paypal-button-container');
-
-                            // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-
-                            // Or go to another URL:  actions.redirect('thank_you.html');
-
-                        });
-
+                            });
                         }
 
                     }).render('#paypal-button-container');
